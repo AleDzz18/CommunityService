e5424aaff9d299f5605fd73d99e71aef79ae283b
AleDzz18 <alejandrojavier12diazmendez@gmail.com>
2025-11-03 14:25:14 -0400

    Agregue el homeDashboard

--- App_Home/views.py
+++ App_Home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def homeDashboard(request):
+    return render(request, 'homeDashboard.html')
 
 def login(request):
     return render(request, 'login.html')
--- Community_Service/settings.py
+++ Community_Service/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Community_Service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
