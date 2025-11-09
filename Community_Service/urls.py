be424279ecf3f167f3270929e2e2ddcfbc6a9c05
AleDzz18 <alejandrojavier12diazmendez@gmail.com>
2025-11-09 18:42:12 -0400

    Creacion de 2 nuevas App

--- Community_Service/urls.py
+++ Community_Service/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App_Home.urls')),
+    path('lider/', include('App_LiderTorre.urls')),
+    path('general/', include('App_LiderGeneral.urls')),
 ]
